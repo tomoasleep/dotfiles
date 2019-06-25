7d5e2d52e42265755244fa60e290cb5832da7c4e
Tomoya Chiba <tomo.asleep@gmail.com>
2019-06-25 22:21:39 +0900

    Stop running history merge before each command

--- fish/config.fish
+++ fish/config.fish
@@ -19,11 +19,6 @@ type -q hub; and eval (hub alias -s)
 
 set PATH $GOROOT/bin $GOPATH/bin $PATH
 
-function history-merge --on-event fish_preexec
-  history save
-  history merge
-end
-
 function git_action_prompt
   if git_is_repo
 		set -l git_action_value (git_action_status)
