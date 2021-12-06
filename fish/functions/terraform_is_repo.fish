9a9383aee09bf03b08bd3982c4cb9741d68d5ed4
Tomoya Chiba <tomo.asleep@gmail.com>
2021-12-06 14:15:38 +0900

    Move configs under config dir

--- fish/functions/terraform_is_repo.fish
+++
@@ -1,9 +0,0 @@
-function terraform_is_repo
-  if test -d .terraform
-    return 0
-  else if test -d terraform/.terraform
-    return 0
-  else
-    return 1
-  end
-end
--- fish/functions/tmux.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function tmux --description 'alias tmux=tmux -2'
-	command tmux -2 $argv;
-end
