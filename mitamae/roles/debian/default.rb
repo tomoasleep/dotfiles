5edbf37bb9c3a62c7395b227076949cf51f57c7a
Tomoya Chiba <tomo.asleep@gmail.com>
2021-12-12 21:48:54 +0900

    Add debian role

---
+++ mitamae/roles/debian/default.rb
@@ -0,0 +1,8 @@
+include_role 'base'
+
+include_cookbook 'apt-build-essentials'
+
+include_cookbook 'homebrew'
+include_cookbook 'devtools'
+
+include_cookbook 'anyenv'
--- mitamae/roles/ubuntu/default.rb
+++ mitamae/roles/ubuntu/default.rb
@@ -1,8 +1,1 @@
-include_role 'base'
-
-include_cookbook 'apt-build-essentials'
-
-include_cookbook 'homebrew'
-include_cookbook 'devtools'
-
-include_cookbook 'anyenv'
+include_role 'debian'
