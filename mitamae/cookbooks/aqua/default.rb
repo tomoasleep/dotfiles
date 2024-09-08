279c2be8bc3aa8c462bd38f451c4f2454c86c08e
Tomoya Chiba <tomo.asleep@gmail.com>
2024-09-08 19:25:03 +0900

    Update aqua-installer to v3.0.1

--- mitamae/cookbooks/aqua/default.rb
+++ mitamae/cookbooks/aqua/default.rb
@@ -1,5 +1,5 @@
 execute "Make ensure aqua is installed" do
-  command '/bin/bash -c "$(curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.0/aqua-installer)"'
+  command '/bin/bash -c "$(curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer)"'
   not_if "which aqua"
 end
 
