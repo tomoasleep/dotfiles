51f8b8d5fe894cddaa52211594b8b652a3c748a0
Tomoya Chiba <tomo.asleep@gmail.com>
2024-09-14 23:09:40 +0900

    Search homebrew bin in mitamae-plugin

--- mitamae/plugins/mitamae-plugin-resource-homebrew/mrblib/resource_executor/homebrew.rb
+++ mitamae/plugins/mitamae-plugin-resource-homebrew/mrblib/resource_executor/homebrew.rb
@@ -4,11 +4,11 @@ module ::MItamae
       class Homebrew < ::MItamae::ResourceExecutor::Base
         def apply
           if !current.exist && desired.exist
-            run_command(["brew", "install", *command_options, desired.target])
+            run_command([homebrew_path, "install", *command_options, desired.target])
           end
 
           if current.exist && !desired.exist
-            run_command(["brew", "uninstall", desired.target])
+            run_command([homebrew_path, "uninstall", desired.target])
           end
         end
 
@@ -36,7 +36,17 @@ module ::MItamae
         end
 
         def brew_list
-          "brew list -1 --full-name"
+          "#{homebrew_path} list -1 --full-name"
+        end
+
+        def homebrew_path
+          if FileTest.file?("/opt/homebrew/bin/brew")
+            "/opt/homebrew/bin/brew"
+          elsif FileTest.file?("/home/linuxbrew/.linuxbrew/bin/brew")
+            "/home/linuxbrew/.linuxbrew/bin/brew"
+          else
+            "brew"
+          end
         end
       end
     end
