16283d47a54ba541e0b035f0de35c67bd0a0259b
Tomoya Chiba <tomo.asleep@gmail.com>
2025-01-19 21:17:32 +0900

    Remove CPATH config because it may add . to include path

--- config/zsh/setup/homebrew.zsh
+++ config/zsh/setup/homebrew.zsh
@@ -1,12 +1,8 @@
 
 if test -f /opt/homebrew/bin/brew; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
-  export CPATH=$CPATH:$(brew --prefix)/include
-  export LIBRARY_PATH=$LIBRARY_PATH:$(brew --prefix)/lib
 fi
 
 if test -f /home/linuxbrew/.linuxbrew/bin/brew; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  export CPATH=$CPATH:$(brew --prefix)/include
-  export LIBRARY_PATH=$LIBRARY_PATH:$(brew --prefix)/lib
 fi
