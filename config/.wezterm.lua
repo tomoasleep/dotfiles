899ed641e07769318a4035cfb5d6bbfa2f410a34
Tomoya Chiba <tomo.asleep@gmail.com>
2025-08-18 15:39:57 +0900

    Update

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font_size = 12
 config.color_scheme = 'Solarized (light) (terminal.sexy)'
 
 return config
