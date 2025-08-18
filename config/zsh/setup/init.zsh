6eed2294a3e8295369be152378c56b4b72e1cd37
Tomoya Chiba <tomo.asleep@gmail.com>
2025-08-18 15:39:39 +0900

    Configure mise on zsh

--- config/zsh/setup/init.zsh
+++ config/zsh/setup/init.zsh
@@ -3,3 +3,4 @@ source ~/.config/zsh/setup/devbin.zsh
 source ~/.config/zsh/setup/aqua.zsh
 source ~/.config/zsh/setup/anyenv.zsh
 source ~/.config/zsh/setup/asdf.zsh
+source ~/.config/zsh/setup/mise.zsh
