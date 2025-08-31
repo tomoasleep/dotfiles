748c5e35eb584edf6455fe9bf6e2e76aabc09026
Tomoya Chiba <tomo.asleep@gmail.com>
2025-09-01 08:52:43 +0900

    Update fonts

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -3,7 +3,13 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font_size = 14
+config.font_size = 15
+config.font = wezterm.font_with_fallback({
+  'HackGen Console NF',
+  'JetBrains Mono',
+  'Noto Color Emoji',
+})
+
 config.color_scheme = 'Solarized (light) (terminal.sexy)'
 
 return config
