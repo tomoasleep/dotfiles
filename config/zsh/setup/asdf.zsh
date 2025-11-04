0fc6ed5e64382f9b8c5c203321bb3878333de4ee
Tomoya Chiba <tomo.asleep@gmail.com>
2025-11-04 09:48:04 +0900

    Update

--- config/zsh/setup/asdf.zsh
+++ config/zsh/setup/asdf.zsh
@@ -1,3 +1,7 @@
-if test -f $HOME/.asdf/asdf.sh; then
-  source "$HOME/.asdf/asdf.sh"
-fi
+# https://asdf-vm.com/guide/upgrading-to-v0-16
+export ASDF_DATA_DIR="$HOME/.asdf"
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
+
+# if test -f $HOME/.asdf/asdf.sh; then
+#   source "$HOME/.asdf/asdf.sh"
+# fi
