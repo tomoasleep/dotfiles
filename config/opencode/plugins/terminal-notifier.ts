d429306d6bae63aa8e0a3a040e460fa4c99e7f02
Tomoya Chiba <tomo.asleep@gmail.com>
2026-02-11 22:58:22 +0900

    Configure notifier on zellij

--- config/opencode/plugins/terminal-notifier.ts
+++ config/opencode/plugins/terminal-notifier.ts
@@ -4,9 +4,11 @@ export const TerminalNotifierPlugin: Plugin = async ({ $ }) => {
   return {
     event: async ({ event }) => {
       if (event.type === "session.idle") {
+        await $`znotify :bell:`;
         await $`terminal-notifier -title "OpenCode" -message "Session completed"`;
       }
       if (event.type === "question.asked") {
+        await $`znotify :question:`;
         await $`terminal-notifier -title "OpenCode" -message "Question requested"`;
       }
     },
