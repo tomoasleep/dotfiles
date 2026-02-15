3231dcf086502b7920223456860fa6f69cfa56d3
Tomoya Chiba <tomo.asleep@gmail.com>
2026-02-15 14:33:38 +0900

    Update

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -75,7 +75,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 
 require("mason").setup()
 require("mason-lspconfig").setup {
-  ensure_installed = { "sorbet" },
+  ensure_installed = {},
 }
 
 vim.api.nvim_create_autocmd('LspAttach', {
