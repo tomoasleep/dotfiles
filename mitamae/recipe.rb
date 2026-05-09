97c20842e0f2a3e69a887e38893dfbfb495cff91
Tomoya Chiba <tomo.asleep@gmail.com>
2026-05-09 14:49:49 +0900

    Support termux

--- mitamae/recipe.rb
+++ mitamae/recipe.rb
@@ -4,4 +4,37 @@ node.reverse_merge!(
   user: ENV['SUDO_USER'] || ENV['USER']
 )
 
-include_role node['platform']
+module ::Specinfra
+  module Helper
+    class DetectOs
+      class Termux < ::Specinfra::Helper::DetectOs
+        def detect
+          if (termux_info = run_command('termux-info')) && termux_info.success?
+            distro = 'termux'
+            release = nil
+            { family: distro, release: release }
+          end
+        end
+      end
+    end
+  end
+end
+
+class ::Specinfra::Command::Termux < ::Specinfra::Command::Debian; end
+
+if node['platform']
+  include_role node['platform']
+elsif (termux_info = run_command('termux-info')) && termux_info.success?
+  class ::Specinfra::Backend::Base
+    def os_info
+      arch = run_command('uname -m').stdout.strip
+      { family: 'termux', release: nil, arch: arch }
+    end
+  end
+
+
+  include_role 'termux'
+else
+  puts 'unsupported OS'
+end
+
